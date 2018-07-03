7dba7479806e0aaddfa09e813a5a5faf2acdb4c5
EBRIONES <e.briones@cgiar.org>
2018-07-03 11:53:18 -0500

    Changing the bundles for style

--- GB-Zone/App_Start/BundleConfig.cs
+++ GB-Zone/App_Start/BundleConfig.cs
@@ -10,26 +10,17 @@ namespace GrinGlobal.Zone
         {
 
             // Homer style
-            bundles.Add(new StyleBundle("~/bundles/homer/css").Include(
-                      "~/Content/style.css", new CssRewriteUrlTransform()));
-            // Animate.css
-            bundles.Add(new StyleBundle("~/bundles/animate/css").Include(
-                      "~/Vendor/animate.css/animate.min.css"));
+            var bundlesContent = new StyleBundle("~/Content/css")
+                                .Include("~/Content/style.css", new CssRewriteUrlTransform())
+                                .Include("~/Content/bootstrap.min.css", new CssRewriteUrlTransform());
 
-            // Pe-icon-7-stroke
-            bundles.Add(new StyleBundle("~/bundles/peicon7stroke/css").Include(
-                      "~/Icons/pe-icon-7-stroke/css/pe-icon-7-stroke.css", new CssRewriteUrlTransform()));
+            var bundlesVendor = new StyleBundle("~/Vendor/css")
+                                 .Include("~/Vendor/animate.css/animate.min.css", new CssRewriteUrlTransform())
+                                 .Include("~/Vendor/pe-icon-7-stroke/css/pe-icon-7-stroke.css", new CssRewriteUrlTransform())
+                                 .Include("~/Vendor/fontawesome/css/font-awesome.min.css", new CssRewriteUrlTransform());
 
-            // Font Awesome icons style
-            bundles.Add(new StyleBundle("~/bundles/font-awesome/css").Include(
-                      "~/Vendor/fontawesome/css/font-awesome.min.css", new CssRewriteUrlTransform()));
-
-
-            // Bootstrap style
-            bundles.Add(new StyleBundle("~/bundles/bootstrap/css")
-                .Include("~/Content/bootstrap.min.css",new CssRewriteUrlTransform())
-                );
-
+            bundles.Add(bundlesContent);
+            bundles.Add(bundlesVendor);
 
             // Homer script
             bundles.Add(new ScriptBundle("~/bundles/homer/js").Include(
